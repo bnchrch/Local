066d73bb8d4a3930b59e96a2a53447c2de8da663
Ben Church <bechurch@Bens-MacBook-Pro.local>
2014-08-28 12:51:13 -0600

    finished splash

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,8 +23,7 @@ console.log(__dirname);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
-app.use(logger('dev'));
+app.use(favicon(__dirname + '/public/assets/favicon.ico'));app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
